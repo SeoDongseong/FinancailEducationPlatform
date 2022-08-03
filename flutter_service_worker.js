623bc5e496553233f58e96c02c13c41b81c2ffb0
powerwarez <powerware@naver.com>
2022-08-03 15:32:09 +0900

    home fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "04259700234cbc50debaa536c06089a2",
-"index.html": "82f0ee3e6e03fb00bad31b351fe90a48",
-"/": "82f0ee3e6e03fb00bad31b351fe90a48",
-"main.dart.js": "9ec72b62d02851699be3015e02e807c7",
+"index.html": "6bfc7c8339894754f7abeb9257679b4d",
+"/": "6bfc7c8339894754f7abeb9257679b4d",
+"main.dart.js": "23b447b6962dc6eca879e0d0986cc223",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
